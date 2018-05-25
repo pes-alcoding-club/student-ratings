99c17b8687bfa03498eabdba68cbef36f7471719
Varun Vora <varunvora@live.com>
2018-05-25 19:27:27 +0530

    Add files via upload

---
+++ json_to_csv.py
@@ -0,0 +1,25 @@
+import csv
+import io
+from json import load
+
+if __name__ == "__main__" :
+	with open("database.json","r") as f:
+		database = load(f)
+	table = []
+	for student in database :
+		row = []
+		row.append(student)
+		row.append(database[student]["name"])
+		row.append(database[student]["rating"])
+		row.append(database[student]["best"])
+		table.append(row[:])
+	table.sort(key = lambda x : x[2])
+	rank = 1
+	for i in range(len(table)) :
+		table[i].insert(0, rank)
+		rank += 1
+
+	with io.open("output.csv",'w', encoding = "utf-8", newline = "") as resultFile:
+		wr = csv.writer(resultFile, dialect='excel')
+		wr.writerows(table)
+	#output of this can be directly put into google sheets
