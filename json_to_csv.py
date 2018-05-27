955b8b83f963e8ad7f3f35a26e7a8512b1fd7a55
Varun Vora <varunvora@live.com>
2018-05-27 17:34:47 +0530

    db update and students who have competed at least once will only appear in ranking

--- json_to_csv.py
+++ json_to_csv.py
@@ -5,15 +5,22 @@ from json import load
 if __name__ == "__main__" :
 	with open("database.json","r") as f:
 		database = load(f)
-	table = [["SRN", "Name", "Rating", "Best"]]
+	table = []
 	for student in database :
-		row = [rank]
+		if database[student]["timesPlayed"] == 0 :
+			continue
+		row = []
 		row.append(student)
 		row.append(database[student]["name"])
 		row.append(database[student]["rating"])
 		row.append(database[student]["best"])
 		table.append(row[:])
-	table.sort(key = lambda x : x[2], reverse = True)
+	table.sort(key = lambda x : x[2])
+	rank = 1
+	for i in range(len(table)) :
+		table[i].insert(0, rank)
+		rank += 1
+	table.insert(0, ["Rank","SRN", "Name", "Rating", "Best"])
 
 	with io.open("output.csv",'w', encoding = "utf-8", newline = "") as resultFile:
 		wr = csv.writer(resultFile, dialect='excel')
