7d1107af6829c9a73ead5a5ed37ba877e24b3c49
Varun Vora <varunvora@live.com>
2018-08-18 01:07:08 +0530

    Isolated ELO functions from RatingProcessor

---
+++ ratings/elo.py
@@ -0,0 +1,90 @@
+"""
+Implementation of ELO rating functions
+Source: https://www.codechef.com/ratings
+"""
+
+from math import log, sqrt
+
+
+def Eab(Ra, Va, Rb, Vb):  # Probability that player A performs worse than player B
+    return 1 / (1 + pow(4, ((Ra - Rb) / (sqrt(pow(Va, 2) + pow(Vb, 2))))))
+
+
+def ERank(Ra, Va, Rb_Vb_list):  # Expected Rank
+    expected_rank = 0
+    for Rb, Vb in Rb_Vb_list:
+        expected_rank += Eab(Ra, Va, Rb, Vb)
+    return expected_rank
+
+
+def Perf(rank, N):  # Performance
+    return log(N / max(0.0001, rank - 0.9999)) / log(4)
+
+
+def EPerf(expected_rank, N):  # Expected Performance
+    return Perf(expected_rank, N)
+
+
+def APerf(actual_rank, N):  # Actual Performance
+    return Perf(actual_rank, N)
+
+
+def Cf(R_list, V_list, N):  # Competition Factor
+    Ravg = sum(R_list) / len(R_list)
+    term1 = sum(map(lambda x: x * x, V_list)) / N
+    term2 = sum(map(lambda x: (x - Ravg) ** 2, R_list)) / (N - 1)
+    return sqrt(term1 + term2)
+
+
+def RWa(timesPlayed):  # Rating weight
+    return (0.4 * timesPlayed + 0.2) / (0.7 * timesPlayed + 0.6)
+
+
+def VWa(timesPlayed):  # Volatility weight
+    return (0.5 * timesPlayed + 0.8) / (timesPlayed + 0.6)
+
+
+def NRa(Ra, APerf, EPerf, Cf, RWa):  # New rating
+    return Ra + (APerf - EPerf) * Cf * RWa
+
+
+def NVa(VWa, NRa, Ra, Va):  # New volatility
+    return sqrt((VWa * ((NRa - Ra) ** 2) + (Va ** 2)) / (VWa + 1.1))
+
+
+def Rcap(Ra, NRa, timesPlayed):  # Cap to how much rating can change
+    cap = 100 + (75 / (timesPlayed + 1)) + ((100 * 500) / (abs(Ra - 1500) + 500))
+    if NRa > Ra:
+        return min(NRa, Ra + cap)
+    return max(NRa, Ra - cap)
+
+
+def Vcap(Va):  # Cap to how much the volatility can change
+    return max(75, min(Va, 200))
+
+
+def process(Ra, Va, timesPlayed, actual_rank, Rb_Vb_list, N, Cf):
+    """
+    :param Ra: Current rating
+    :param Va: Current volatility
+    :param timesPlayed: Current timesPlayed
+    :param actual_rank: Actual rank in the contest
+    :param Rb_Vb_list: Rating and volatility of remaining players
+    :param N: Total players
+    :param Cf: Competition factor
+    :return: 2-tuple of new rating and new volatility
+    """
+
+    new_rating = NRa(Ra,
+                     APerf(actual_rank, N),
+                     EPerf(ERank(Ra, Va, Rb_Vb_list), N),
+                     Cf,
+                     RWa(timesPlayed)
+                     )
+
+    new_volatility = NVa(VWa(timesPlayed), new_rating, Ra, Va)
+
+    new_rating = Rcap(Ra, new_rating, timesPlayed)
+    new_volatility = Vcap(new_volatility)
+
+    return new_rating, new_volatility
--- ratings/rating_processor.py
+++ ratings/rating_processor.py
@@ -1,96 +1,44 @@
 import logging
-from math import sqrt, log
+from ratings import elo
 
-# this should be in db column
+# this should be in db folder
 JSON_RATING = 'rating'
 JSON_VOL = 'volatility'
 JSON_TIMES = 'timesPlayed'
+JSON_BEST = 'best'
 
 
 class RatingProcessor:
-    """
-    Takes a list of student
-    """
+
     def __init__(self, player_dict_list):
+        """
+        Takes a list of student
+        """
         try:
             assert isinstance(player_dict_list, list)
             assert all(isinstance(x, dict) for x in player_dict_list)
             assert all(JSON_RATING in x for x in player_dict_list)
             assert all(JSON_VOL in x for x in player_dict_list)
+            assert all(JSON_TIMES in x for x in player_dict_list)
+            assert all(JSON_BEST in x for x in player_dict_list)
 
         except AssertionError:
             logging.error('Invalid input to RatingProcessor')
+        self.N = None
+        self.Cf = None
+        self.Rb_Vb_list = None
 
-    class ELO:
-        """
-        Implementation of ELO rating functions
-        Source: https://www.codechef.com/ratings
-        Comments contain older implementations
-        """
+    def process_player(self, player_dict, actual_rank):
 
-        @staticmethod
-        def Erank(Ra, Va, Rb_Vb_list):  # Expected Rank
-            expected_rank = 0
+        old_rating = player_dict[JSON_RATING]
+        old_volatility = player_dict[JSON_VOL]
+        times_played = player_dict[JSON_TIMES]
+        old_best = player_dict[JSON_BEST]
 
-            def Eab(Ra, Va, Rb, Vb):  # Probability that player A performs worse than player B
-                return 1 / (1 + pow(4, ((Ra - Rb) / (sqrt(pow(Va, 2) + pow(Vb, 2))))))
+        new_rating, new_volatility = elo.process(
+            old_rating, old_volatility, times_played, actual_rank, self.Rb_Vb_list, self.N, self.Cf)
 
-            for Rb, Vb in Rb_Vb_list:
-                expected_rank += Eab(Ra, Va, Rb, Vb)
-            return expected_rank
-
-        @staticmethod
-        def Perf(rank, N):  # Performance
-            return log(N / max(0.0001, rank - 0.9999)) / log(4)
-
-        # Perf = lambda rank, N :  log(N/(rank-1)) / log(4) causes division by zero
-        # Perf = lambda rank, N: log(N / (max(0.01, rank - 0.99))) / log(4)
-
-        @classmethod
-        def EPerf(cls, ERank, N):  # Expected Performance
-            return cls.Perf(ERank, N)
-
-        @classmethod
-        def APerf(cls, ARank, N):  # Actual Performance
-            return cls.Perf(ARank, N)
-
-        @staticmethod
-        def Cf(R_list, V_list, N):  # Competition Factor
-            Ravg = sum(R_list) / len(R_list)
-            term1 = sum(map(lambda x: x * x, V_list)) / N
-            term2 = sum(map(lambda x: (x - Ravg) ** 2, R_list)) / (N - 1)
-            return sqrt(term1 + term2)
-
-        @staticmethod
-        def RWa(timesPlayed):
-            return (0.4 * timesPlayed + 0.2) / (0.7 * timesPlayed + 0.6)
-
-        # RWa = lambda timesPlayed: (0.4 * timesPlayed + 0.2) / (0.7 * timesPlayed + 0.6)
-
-        @staticmethod
-        def VWa(timesPlayed):
-            return (0.5 * timesPlayed + 0.8) / (timesPlayed + 0.6)
-
-        # VWa = lambda timesPlayed: (0.5 * timesPlayed + 0.8) / (timesPlayed + 0.6)
-        @staticmethod
-        def NRa(Ra, APerf, EPerf, Cf, RWa):
-            return Ra + (APerf - EPerf) * Cf * RWa
-
-        # NRa = lambda Ra, APerf, EPerf, Cf, RWa: Ra + (APerf - EPerf) * Cf * RWa
-
-        @staticmethod
-        def NVa(VWa, NRa, Ra, Va):
-            return sqrt((VWa * ((NRa - Ra) ** 2) + (Va ** 2)) / (VWa + 1.1))
-
-        # NVa = lambda VWa, NRa, Ra, Va: sqrt((VWa * ((NRa - Ra) ** 2) + (Va ** 2)) / (VWa + 1.1))
-
-        @staticmethod
-        def Rcap(Ra, timesPlayed):
-            return 100 + (75 / (timesPlayed + 1)) + ((100 * 500) / (abs(Ra - 1500) + 500))
-
-        # Rcap = lambda Ra, timesPlayed: 100 + (75 / (timesPlayed + 1)) + ((100 * 500) / (abs(Ra - 1500) + 500))
-
-        @staticmethod
-        def Vcap(Va):
-            return max(75, min(Va, 200))
-        # Vcap = lambda Va: max(75, min(Va, 200))  # (75, 200) #the volatility can change maximum by these two
+        player_dict[JSON_RATING] = new_rating
+        player_dict[JSON_VOL] = new_volatility
+        player_dict[JSON_TIMES] = times_played + 1
+        player_dict[JSON_BEST] = max(old_best, new_rating)
