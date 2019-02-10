47385867dcd42b93f9a3cb8c3e25d1921b57fe83
varunvora <varunvora@live.com>
2019-02-10 23:08:51 +0530

    updated ratings from October 2018 to January 2019

--- executor.sh
+++ executor.sh
@@ -4,22 +4,24 @@ echo "Starting executor"
 
 export PYTHONPATH="${PYTHONPATH}../alcoding"
 
-python database/db_tools.py reset_database
+python3 database/db_tools.py reset_database
 echo "Finished Database Reset"
 
 # List all contests ranks here in chronological order
-python ratings/processor.py database/contest_ranks/codejam-qualification.in codejam
-python ratings/processor.py database/contest_ranks/codejam-1a.in codejam
-python ratings/processor.py database/contest_ranks/codejam-1b.in codejam
-python ratings/processor.py database/contest_ranks/codejam-1c.in codejam
-python ratings/processor.py database/contest_ranks/alcoding2018.in hackerearth
-python ratings/processor.py database/contest_ranks/codechef-oct18.in codechef
-python ratings/processor.py database/contest_ranks/codechef-nov18.in codechef
-python ratings/processor.py database/contest_ranks/codechef-dec18.in codechef
+python3 ratings/processor.py database/contest_ranks/codejam-qualification.in
+python3 ratings/processor.py database/contest_ranks/codejam-1a.in
+python3 ratings/processor.py database/contest_ranks/codejam-1b.in
+python3 ratings/processor.py database/contest_ranks/codejam-1c.in
+python3 ratings/processor.py database/contest_ranks/alcoding2018.in
+python3 ratings/processor.py database/contest_ranks/codechef-oct18.in
+python3 ratings/processor.py database/contest_ranks/codechef-nov18.in
+python3 ratings/processor.py database/contest_ranks/codechef-dec18.in
+python3 ratings/processor.py database/contest_ranks/hackerearth-jan-easy-2019.in
+python3 ratings/processor.py database/contest_ranks/codechef-jan19.in
 
 echo "Finished Ratings Update"
 
-python database/db_tools.py export_to_csv
+python3 database/db_tools.py export_to_csv
 echo "Exported Scoreboard from database. You can now quit."
 
 read # Prevents the terminal from closing
