c4648694f3063dc059c56795da49984407b8e871
souravtecken <souravtecken@gmail.com>
2019-03-07 22:57:50 +0530

    Ran scraper for codeJam 2018 qualifiers. 1. Scraper works for both codeJam and kickstart. 2. Created codejam-qualifiers-2018.in

--- scrapers/google.py
+++ scrapers/google.py
@@ -2,15 +2,41 @@ from selenium import webdriver
 from time import sleep
 from csv import writer
 
-IS_OUTPUT_CSV = True
+IS_OUTPUT_CSV = False
 
-scoreboard_url = "https://codingcompetitions.withgoogle.com/codejam/round/0000000000007766"
+scoreboard_url = "https://codingcompetitions.withgoogle.com/codejam/round/00000000000000cb"
+
 chromeOptions = webdriver .ChromeOptions()
 prefs = {'profile.managed_default_content_settings.images': 2,  # does not load images on web page
          'disk-cache-size': 1024}  # use disk cache to reduce page load time
+
 chromeOptions.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(options=chromeOptions)
 driver.get(scoreboard_url)
+
+
+
+'''
+<div class="ranking-table__row">
+    <div class="ranking-table__row-cell ranking-table__row-cell--left ranking-table__row-cell__rank">1</div>
+        <div class="ranking-table__row-cell ranking-table__row-cell--left ranking-table__row-cell__displayname">
+            <a href="/codejam/submissions/00000000000000cb/eTAxMDV3NDk" class="ranking-table__row-cell__submissions_link">
+                <p><img src="/static/Canada-20.png" class="ranking-table__row-cell-flag flag has-tooltip" />y0105w49 </p>                                    
+            </a>
+        </div>
+    <div class="ranking-table__row-cell ranking-table__row-cell--left ranking-table__row-cell__score">
+        <span class="user-total-score">100</span>
+    </div>
+</div>
+
+
+Each record in the scoreboard is of this format.
+Rank is stored directly in the <div> with className="ranking-table__row-cell__rank"
+Score is stored directly in the <span> with className="user-total-score"
+UserName is stored in a <p> which is the child element of the <a> with className="ranking-table__row-cell__displayname"
+
+'''
+
 
 score_class = "user-total-score"
 rank_class = "ranking-table__row-cell__rank"
@@ -21,8 +47,13 @@ scraped_scoreboard = list()
 while not driver.find_elements_by_class_name(score_class):
     sleep(1)
 
+# Pause while user changes default page view to 20 rows - useful for large scoreboard size
+# Make necessary changes in page, press enter to continue
+input()
+
 # Find number of pages in the scoreboard
 total_pages = int(driver.find_element_by_class_name("ranking-table-page-number-total-pages").text.split()[1])
+
 
 for page in range(total_pages):
     score_elements = driver.find_elements_by_class_name(score_class)
@@ -38,6 +69,7 @@ for page in range(total_pages):
     last_name = scraped_scoreboard[-1][0]
     while driver.find_elements_by_class_name(name_class)[-1].find_element_by_tag_name("p").text == last_name:
         sleep(0.1)  # Wait until next page has loaded
+
 driver.close()
 
 if IS_OUTPUT_CSV:
@@ -45,4 +77,4 @@ if IS_OUTPUT_CSV:
         csv_writer = writer(fp)
         csv_writer.writerows(scraped_scoreboard)
 else:
-    print(*[x[0] for x in scraped_scoreboard], sep="\n")
+    print(*[x[0] for x in scraped_scoreboard], sep=" ")
