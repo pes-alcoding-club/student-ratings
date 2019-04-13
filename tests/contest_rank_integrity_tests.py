715c8638392c33ee0559c439080aecd1d45632c8
souravtecken <souravtecken@gmail.com>
2019-04-14 00:45:55 +0530

    Updated Codejam 2019 - qualifers and 1a ratings. 1. Updated ratings for codejam 2019 qualifers and round 1a 2. Added appropriate assertion message in contest_rank_integrity_tests.py

--- tests/contest_rank_integrity_tests.py
+++ tests/contest_rank_integrity_tests.py
@@ -10,21 +10,23 @@ class ContestsRanksIntegrityTests(unittest.TestCase):
         for file_path in listdir(CONTEST_RANKS_DIR):
             with open(path.join(CONTEST_RANKS_DIR, file_path)) as fp:
                 usn_or_handle_list = fp.read().split()
-            self.assertEqual(len(usn_or_handle_list), len(set(usn_or_handle_list)))
+            self.assertEqual(len(usn_or_handle_list), len(set(usn_or_handle_list)),msg="Repeated users in file {}".format(file_path))
 
     def test_valid_entries(self):
         for file_path in listdir(CONTEST_RANKS_DIR):
             with open(path.join(CONTEST_RANKS_DIR, file_path)) as fp:
                 usn_or_handle_list = fp.read().split()
             for usn_or_handle in usn_or_handle_list:
-                self.assertTrue(VALID_USN_REGEX.match(usn_or_handle) or VALID_USERNAME_REGEX.match(usn_or_handle))
+                self.assertTrue(VALID_USN_REGEX.match(usn_or_handle) or VALID_USERNAME_REGEX.match(usn_or_handle),msg="Invalid \
+                USN or username {} in file {}".format(usn_or_handle,file_path))
 
     def test_non_empty_lines(self):
         for file_path in listdir(CONTEST_RANKS_DIR):
             with open(path.join(CONTEST_RANKS_DIR, file_path)) as fp:
+                line_number:int=1
                 for line in fp.readlines():
-                    self.assertNotEqual(line.strip(), "")
-
+                    self.assertNotEqual(line.strip(), "",msg="Empty line - {} in {}".format(line_number,file_path))
+                    line_number+=1
 
 if __name__ == '__main__':
     unittest.main()
